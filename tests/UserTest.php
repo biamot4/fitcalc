18d9bc2daab56d36a9108636c9d5b1894c72fd69
biamot4 <beatrizmotasesi@gmail.com>
2025-10-01 12:17:58 -0300

    feat: Teste de usuário

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -17,24 +17,65 @@ class Usertest extends TestCase {
 
 
     #[\PHPUnit\Framework\Attributes\Test]
-    public function it_should_be_able_to_crteate_user(){
+    public function it_should_be_able_to_create_user(){
+        $this->mockUserModel->method('registerUser')->willReturn(True);
         $userResult = $this->userController->createUser('Ana Luisa Santos', 'beatriz@example.com', '123456');
         $this->assertTrue($userResult);
     }
 
     #[\PHPUnit\Framework\Attributes\Test]
     public function it_should_be_able_to_sign_in(){
-        $this->mockUserModel->method('getuserByEmail')->willReturn([
+
+        $this->mockUserModel->method('getUserByEmail')->willReturn([
+            "id"=>1,
+            "user_fullname" => "Ana Luisa Santos",
+            "email"=> "ana@example.com",
+            
+            "password"=> password_hash(123456, PASSWORD_DEFAULT),
+        ]);
+
+        $userResult = $this->userController->login("ana@example.com", "123456");
+        $this->assertTrue($userResult);
+        $this->assertEquals(1, $_SESSION["id"],);
+        $this->assertEquals('Ana Luisa Santos', $_SESSION["user_fullname"],);
+        $this->assertEquals('ana@example.com', $_SESSION["email"]);
+    }
+
+
+    #[\PHPUnit\Framework\Attributes\Test]
+    public function it_shouldnt_login_with_invalid_credentials(){
+
+            $this->mockUserModel->method('getuserByEmail')->willReturn([
             "id"=>1,
             "user _fullname" => "Ana Luisa Santos",
             "email"=> "ana@example.com",
             "password"=> password_hash(123456, PASSWORD_DEFAULT),
         ]);
+        
+        $userResult = $this->userController->login("ana@example.com", "12345");
+        $this->assertFalse($userResult);
+    }
+
+
+    #[PHPUnit\Framework\Attributes\Test]
+    public function it_should_be_able_to_check_user_by_email(){
+         $this->mockUserModel->method('getuserByEmail')->willReturn([
+            "id"=>1,
+            "user _fullname" => "Ana Luisa Santos",
+            "email"=> "ana@example.com",
+            "password"=> '$2y$10$wwh8v4aP/UFlocuM0NIk9.tIg6mH.pe4.CkcMLKLlhMVibpF3Uf6q',
+        ]);
+
+        $userResult = $this->userController->checkUserByEmail('ana@example.com');
+        $this->assertNotNull($userResult);
+        $this->assertEquals('ana@example.com', $userResult['email']);
+    }
+
+    #[\PHPUnit\Framework\Attributes\Test]
+    public function it_should_verify_if_is_logged_in(){
+    $_SESSION["id"]= 1;
+    $userResult = $this->userController->isLoggedIn();
+    $this->assertTrue($userResult);
     }
 }
-
-
-
-
-
 ?>
---
+++ tests/teste.php
@@ -0,0 +1,7 @@
+<?php
+
+$password = '123456';
+$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+echo $hashedPassword;
+
+?>
