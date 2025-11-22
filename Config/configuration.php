eb58dd13b03548dc1356626aa382228a139a0d29
biamot4 <beatrizmotasesi@gmail.com>
2025-11-22 17:25:09 -0300

    Correção para hospedagem no arquivo configuration.php

--- Config/configuration.php
+++ Config/configuration.php
@@ -2,7 +2,7 @@
 // CONFIGURAÇÃO DO BANCO DE DADOS
 
 define("DB_NAME", "fitcalc");
-define("DB_HOST", "localhost");
+define("DB_HOST", "127.0.0.1");
 define("DB_USER", "root");
 define("DB_PASSWORD", '');
 define("DB_PORT", "3306");
