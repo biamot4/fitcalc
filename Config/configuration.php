ec1b86c9241ece60b0267bd78f389ad7e72ac612
biamot4 <beatrizmotasesi@gmail.com>
2025-11-22 17:37:38 -0300

    Correção para hospedagem no arquivo configuration.php

--- Config/configuration.php
+++ Config/configuration.php
@@ -1,9 +1,17 @@
 <?php
-// CONFIGURAÇÃO DO BANCO DE DADOS
+$servername = "127.0.0.1";
+$username = "root"; // Seu usuário
+$password = ""; // Sua senha (vazia)
+$dbname = "fitcalc"; // Seu nome do banco de dados
+$port = 3306; // Sua porta
 
-define("DB_NAME", "fitcalc");
-define("DB_HOST", "127.0.0.1");
-define("DB_USER", "root");
-define("DB_PASSWORD", '');
-define("DB_PORT", "3306");
+// Tenta Conexão
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+// Verifica Conexão
+if ($conn->connect_error) {
+    die("A conexão falhou. Erro: " . $conn->connect_error);
+}
+echo "Conexão bem-sucedida! O problema está na classe PDO/Connection.";
+$conn->close();
 ?>
